d8ca67bd81763ace2c8fdf56677d3c6cd5795be9
Jeffrey Oomen <jeffrey_oomen@outlook.com>
2017-11-07 13:58:34 +0100

    initialized spring boot app

---
+++ src/main/java/com/infosupport/minor/rabbit_mq_restaurant_app/RabbitMqRestaurantAppApplication.java
@@ -0,0 +1,12 @@
+package com.infosupport.minor.rabbit_mq_restaurant_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqRestaurantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqRestaurantAppApplication.class, args);
+	}
+}
